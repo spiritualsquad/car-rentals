4d5fcdc65dc66677b83c965c0c7a594236b42120
spiritualsquad <dondikshu@gmail.com>
2024-05-07 15:49:51 -0400

    Changed Cars

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
         Car car2 = new Car("C002", "Honda", "Accord", 70.0);
-        Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+        Car car3 = new Car("C003", "Tesla", "X", 300.0);
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
